e17e025155d0cf15e30042b8d46c6615b2ed82f6
alwayswannabekind <99820844+alwayswannabekind@users.noreply.github.com>
2022-05-01 12:37:56 +0300

    5

---
+++ Lesson6/Server.java
@@ -0,0 +1,79 @@
+package Lesson6;
+
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+
+    private static final int PORT = 8189;
+
+    public static void main(String[] args) throws IOException {
+        new Server().start(PORT);
+    }
+
+    public void start(int port) throws IOException {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        Thread inputThread = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server is up");
+            clientSocket = serverSocket.accept();
+            System.out.println("Client has been connected!");
+            DataInputStream inputStream = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+            inputThread = runInputLoop(inputStream);
+            runOutputLoop(outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputThread != null) {
+                inputThread.interrupt();
+            }
+            if (clientSocket != null) {
+                clientSocket.close();
+            }
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        }
+    }
+
+    private Thread runInputLoop(DataInputStream inputStream) {
+        Thread thread = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    String message = inputStream.readUTF();
+                    System.out.println("From client: " + message);
+                    if (message.startsWith("/end")) {
+                        System.exit(0);
+                    }
+                } catch (IOException e) {
+                    System.out.println("Connection lost");
+                    System.exit(0);
+                    break;
+                }
+            }
+
+        });
+        thread.start();
+        return thread;
+    }
+
+    private void runOutputLoop(DataOutputStream outputStream) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String message = scanner.nextLine();
+            if (message.startsWith("/end")) {
+                break;
+            }
+            outputStream.writeUTF(message);
+        }
+    }
+}
